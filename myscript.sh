80671d184c5c70f4364375d2c40266da981d33b1
Denis <88098382+NonkeQA@users.noreply.github.com>
2021-09-04 15:53:30 +0200

    Delete myscript.sh

--- myscript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd hw1
-mkdir {folder1,folder2,folder3}
-cd folder1
-touch file1.txt file2.txt file3.txt file4.json file5.json
-mkdir {papka1,papka2,papka3}
-ls
-mv file1.txt file4.json papka2
-
