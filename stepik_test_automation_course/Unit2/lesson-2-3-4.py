2711cfb0f429b29af38c63f76b96753b0159ce35
Denis Ruban <nonkepz@gmail.com>
2022-08-03 22:28:50 +0300

    accept alert

---
+++ stepik_test_automation_course/Unit2/lesson-2-3-4.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import time
+import math
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+browser = webdriver.Chrome()
+browser.get(link)
+
+button = browser.find_element_by_class_name("btn-primary")
+button.click()
+alert = browser.switch_to.alert
+alert.accept()
+x_number = browser.find_element_by_id("input_value")
+x = x_number.text
+y = calc(x)
+input = browser.find_element_by_id("answer")
+input.send_keys(y)
+button = browser.find_element_by_class_name("btn-primary")
+button.click()
+
+time.sleep(5)
+browser.quit()
